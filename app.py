700d1344c2c00343be077231867e96a21b097894
Vinicius-Giovanni <viniciusgiovanni2003@gmail.com>
2026-05-13 15:09:18 -0300

    Usando fast api em vez de flask

--- app.py
+++ app.py
@@ -1,29 +1,48 @@
-from flask import Flask, render_template, jsonify
+from fastapi import FastAPI
+from fastapi.requests import Request
+from fastapi.responses import HTMLResponse
+from fastapi.responses import JSONResponse
+from fastapi.templating import Jinja2Templates
+
 from automacao import emitir_relatorio
+
 import threading
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
+import uvicorn
 
 
-@app.route('/emitir-relatorio', methods=['POST'])
+app = FastAPI()
+
+templates = Jinja2Templates(directory='templates')
+
+
+@app.get('/', response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name='index.html',
+        context={}
+    )
+
+
+@app.post('/emitir-relatorio')
 def emitir():
 
-    # roda em thread separada
-    thread = threading.Thread(target=emitir_relatorio)
+    thread = threading.Thread(
+        target=emitir_relatorio
+    )
+
     thread.start()
 
-    return jsonify({
-        "mensagem": "Automação iniciada"
+    return JSONResponse({
+        'mensagem': 'Automação iniciada'
     })
 
 
 if __name__ == '__main__':
-    app.run(
+
+    uvicorn.run(
+        'app:app',
         host='0.0.0.0',
         port=5000,
-        debug=True
+        reload=True
     )
