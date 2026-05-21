1c1865c26d880ccd66220894e9d17b665973c147
Vinicius-Giovanni <viniciusgiovanni2003@gmail.com>
2026-05-21 10:19:41 -0300

    style: página inicial finalizada

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -5,12 +5,21 @@ class MobileNavbar {
         this.navLinks = document.querySelectorAll(navLinks);
         this.activeClass = "active";
 
-        this.handleClick = this.handleClick.bind();
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+        });
     }
 
     handleClick() {
-        console.log(this);
         this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
     }
 
     addClickEvent() {
